eee7f144a21a6ce141bbee1105e1fa22bcff60b5
aabubakar17 <abubakarabdihakim77@gmail.com>
2024-01-15 18:13:51 +0000

    added get /api/articles/:article_id request

--- Controller/app.controller.js
+++ Controller/app.controller.js
@@ -1,7 +1,10 @@
-const { fetchTopics, fetchEndpoints } = require("../Model/app.model");
+const {
+  fetchTopics,
+  fetchEndpoints,
+  findArticleById,
+} = require("../Model/app.model");
 
 exports.getTopics = (req, res) => {
-  console.log(req.route);
   fetchTopics().then((topics) => {
     res.status(200).send({ topics });
   });
@@ -11,4 +14,16 @@ exports.getEndpoints = (req, res) => {
   fetchEndpoints().then((resEndpoints) => {
     res.status(200).send({ resEndpoints });
   });
+};
+
+exports.getArticleById = (req, res, next) => {
+  const { article_id } = req.params;
+  findArticleById(article_id)
+    .then((article) => {
+      res.status(200).send({ article });
+    })
+    .catch((err) => {
+      console.log(err);
+      next(err);
+    });
 };
